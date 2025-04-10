4d9d152232ab1345e52ec78a4551ae8f3a49ef77
Eduardo Moura Leonardi <100094001+EduardoMouraLeonardi@users.noreply.github.com>
2025-04-10 19:40:48 -0300

    CREATE

---
+++ WEB/Aula_03/aula_03.js
@@ -0,0 +1,23 @@
+/*var num = window.prompt("Digite um número: ");
+
+if (num % 2 == 0 && num != 0) {
+    window.alert("O número " + num + " é par.");
+}else if (num % 2 != 0) {
+    window.alert("O número " + num + " é ímpar.");
+}else {
+    window.alert("O número " + num + " é zero.");
+}
+
+var num2 = window.prompt("Digite outro número: ");
+
+if (num < 0) {
+    window.alert("O número " + num2 + " é negativo.");
+}else if (num2 > 0) {
+    window.alert("O número " + num2 + " é positivo.");
+}
+else {
+    window.alert("O número " + num2 + " é zero.");
+}
+*/
+
+
